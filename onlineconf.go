456b7168b0042c0a7fd8f8a2768c7478202f82a1
Timofey Khryukin <khryukin@corp.mail.ru>
2017-07-10 14:28:31 +0300

    additional methods in iproto and onlineconf

--- onlineconf.go
+++ onlineconf.go
@@ -1,134 +1,145 @@
 package onlineconf
 
-import(
-    "os"
-    "fmt"
-    "log"
-    "bufio"
-    "strings"
-    "strconv"
-    "github.com/howeyc/fsnotify"
+import (
+	"bufio"
+	"fmt"
+	"github.com/howeyc/fsnotify"
+	"log"
+	"os"
+	"path"
+	"strconv"
+	"strings"
 )
 
 type row struct {
-    i int
-    c bool
-    s string
+	i int
+	c bool
+	s string
 }
 
 var cfg map[string]*row = make(map[string]*row)
-var file string = "/usr/local/etc/onlineconf/TREE.conf"
+
+//var file string = "/usr/local/etc/onlineconf/TREE.conf"
+var config_file string = fmt.Sprintf("/usr/local/etc/onlineconf/%s.conf", path.Base(os.Args[0]))
 
 func init() {
-    read()
+	read()
 
-    go func() {
-        watcher, err := fsnotify.NewWatcher()
+	go func() {
+		watcher, err := fsnotify.NewWatcher()
 
-        defer watcher.Close()
+		defer watcher.Close()
 
-        if err != nil {
-            panic(err)
-        }
+		if err != nil {
+			panic(err)
+		}
 
-        err = watcher.Watch(file)
+		err = watcher.Watch(config_file)
 
-        if err != nil {
-            panic(err)
-        }
+		if err != nil {
+			panic(err)
+		}
 
-        for {
-            select {
-                case ev := <-watcher.Event:
-                    err := watcher.RemoveWatch(file)
+		for {
+			select {
+			case ev := <-watcher.Event:
+				err := watcher.RemoveWatch(config_file)
 
-                    if err != nil {
-                        panic(err)
-                    }
+				if err != nil {
+					panic(err)
+				}
 
-                    if ev.IsCreate() || ev.IsModify() {
-                        read()
-                    }
+				if ev.IsCreate() || ev.IsModify() {
+					read()
+				}
 
-                    err = watcher.Watch(file)
+				err = watcher.Watch(config_file)
 
-                    if err != nil {
-                        panic(err)
-                    }
-                case err := <-watcher.Error:
-                    log.Printf("Watch %v error: %v\n", file, err)
-            }
-        }
-    }()
+				if err != nil {
+					panic(err)
+				}
+			case err := <-watcher.Error:
+				log.Printf("Watch %v error: %v\n", config_file, err)
+			}
+		}
+	}()
 }
 
 func read() {
-    log.Printf("Start read file %v\n", file)
+	log.Printf("Start read file %v\n", config_file)
 
-    f, err := os.Open(file)
+	f, err := os.Open(config_file)
 
-    if err != nil {
-        log.Printf("Open file %v error %v\n", file, err)
-        return
-    }
+	if err != nil {
+		log.Printf("Open file %v error %v\n", config_file, err)
+		return
+	}
 
-    defer f.Close()
+	defer f.Close()
 
-    buff := make([]string, 2)
-    scan := bufio.NewScanner(f)
-  
-    for scan.Scan() {
-        if buff = strings.SplitN(scan.Text(), " ", 2); len(buff) == 2 {
-            if val, ok := cfg[buff[0]]; ok {
-                if val.s != buff[1] {
-                    val.c = false
-                    val.s = buff[1]
-                }
-            } else {
-                cfg[buff[0]] = &row{
-                    c: false,
-                    s: buff[1],
-                }
-            }
-        }
-    }
+	buff := make([]string, 2)
+	scan := bufio.NewScanner(f)
 
-    log.Printf("Finish read file %v\n", file)
+	for scan.Scan() {
+		if buff = strings.SplitN(scan.Text(), " ", 2); len(buff) == 2 {
+			if val, ok := cfg[buff[0]]; ok {
+				if val.s != buff[1] {
+					val.c = false
+					val.s = buff[1]
+				}
+			} else {
+				cfg[buff[0]] = &row{
+					c: false,
+					s: buff[1],
+				}
+			}
+		}
+	}
+
+	log.Printf("Finish read file %v\n", config_file)
 }
 
 func GetAsInt(k string, d ...int) int {
-    if val, ok := cfg[k]; ok {
-        if val.c == true {
-            return val.i
-        }
+	if val, ok := cfg[k]; ok {
+		if val.c == true {
+			return val.i
+		}
 
-        i, err := strconv.Atoi(val.s)
+		i, err := strconv.Atoi(val.s)
 
-        if err != nil {
-            log.Printf("strconv.Atoi key %s error %v\n", k, file)
-        } else {
-            val.i = i
-            val.c = true
+		if err != nil {
+			log.Printf("strconv.Atoi key %s error %v\n", k, config_file)
+		} else {
+			val.i = i
+			val.c = true
 
-            return i
-        }
-    }
+			return i
+		}
+	}
 
-    if len(d) >= 1 {
-        return d[0]
-    }
+	if len(d) >= 1 {
+		return d[0]
+	}
 
-    panic(fmt.Sprintf("%v key not exist and default not found", k))
+	panic(fmt.Sprintf("%v key not exist and default not found", k))
 }
 
 func GetAsString(k string, d ...string) string {
-    if val, ok := cfg[k]; ok {
-        return val.s
-    }
+	if val, ok := cfg[k]; ok {
+		return val.s
+	}
 
-    if len(d) >= 1 {
-        return d[0]
-    }
+	if len(d) >= 1 {
+		return d[0]
+	}
 
-    panic(fmt.Sprintf("%v key not exist and default not found", k))
+	panic(fmt.Sprintf("%v key not exist and default not found", k))
+}
+
+func IfExistAsString(k string) (string, bool) {
+	if val, ok := cfg[k]; ok {
+		return val.s, ok
+	}
+
+	return "", false
 }
