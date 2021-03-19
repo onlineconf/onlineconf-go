ae63d12bdef21d90d737f5a02d8663dd991fe411
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2021-03-19 15:15:13 +0300

    add -bool and -module-dir option

--- cmd/onlineconf-get/main.go
+++ cmd/onlineconf-get/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -12,19 +13,37 @@ import (
 
 func main() {
 
-	isInteractive := flag.Bool("interactive", false, "run get onlineconf in interactive mode")
-	ocModuleName := flag.String("module", "TREE", "onlineconf module")
+	isInteractive := flag.Bool("interactive", false, "Run get onlineconf in interactive mode")
+	ocModuleDir := flag.String("module-dir", "/usr/local/etc/onlineconf", "Onlineconf module dir")
+	ocModuleName := flag.String("module", "TREE", "Onlineconf module")
+	asBool := flag.Bool("bool", false, "Interpret value as boolean and exit with code 0 on true and 1 on false. Only non-interactive mode")
 
 	flag.Parse()
 
+	if *asBool && *isInteractive {
+		fmt.Fprintln(os.Stderr, "-bool option i not available in interactive mode")
+		os.Exit(2)
+	}
+
+	onlineconf.Initialize(*ocModuleDir)
+	onlineconf.SetOutput(os.Stderr)
 	module := onlineconf.GetModule(*ocModuleName)
 
 	if !*isInteractive {
 		if flag.CommandLine.NArg() != 1 {
 			fmt.Println("path argiment is required for non interactive mode")
-			os.Exit(1)
+			os.Exit(2)
 		}
 		onlineconfPath := flag.CommandLine.Arg(0)
+
+		if *asBool {
+			value := module.GetBool(onlineconfPath, false)
+			if value {
+				os.Exit(0)
+			}
+			os.Exit(1)
+		}
+
 		readOCPath(module, onlineconfPath)
 
 		return
@@ -33,7 +52,13 @@ func main() {
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter onlineconf path: ")
-		onlineconfPath, _ := reader.ReadString('\n')
+		onlineconfPath, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
 		onlineconfPath = strings.Trim(onlineconfPath, "\n")
 		readOCPath(module, onlineconfPath)
 	}
@@ -47,5 +72,5 @@ func readOCPath(module *onlineconf.Module, path string) {
 		return
 	}
 
-	fmt.Println("No such key")
+	fmt.Fprintln(os.Stderr, "No such key")
 }
